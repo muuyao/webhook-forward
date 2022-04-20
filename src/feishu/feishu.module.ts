85e8a5f7d8c001406cbe97e120b1de427867f303
wangxiao <wanngxiao@gmail.com>
2022-04-20 10:47:19 +0800

    init commit

--- src/feishu/feishu.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FeishuController } from './feishu.controller';
-import { FeishuService } from './feishu.service';
-
-@Module({
-  controllers: [FeishuController],
-  providers: [FeishuService],
-})
-export class FeishuModule {}
--- src/feishu/feishu.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class FeishuService {
-  receiver(): string {
-    return 'Hello World! feishu';
-  }
-}
