f0021e9948039b8b944cf3f468e79a5a74a5b27c
wangxiao <wanngxiao@gmail.com>
2022-04-20 11:32:22 +0800

    feat: add post

--- pages/api/receiver.ts
+++ pages/api/receiver.ts
@@ -1,10 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
 type Data = {
   name: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe', ...req.body });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const body = req.body;
+  const headers = req.headers;
+
+  if (headers['X-Gitlab-Event']) {
+    const { event_type, user, repository, object_attributes } = body;
+    await axios.post('https://open.feishu.cn/open-apis/bot/v2/hook/db3c5890-fe9d-451f-818d-0d06193c5c28', {
+      msg_type: 'post',
+      content: {
+        post: {
+          zh_cn: {
+            title: repository.name + ' ' + event_type,
+            content: [
+              [
+                {
+                  tag: 'text',
+                  text: '提交人: ' + user.name,
+                },
+                {
+                  tag: 'a',
+                  text: '请查看',
+                  href: object_attributes.url,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    });
+  }
+
+  res.status(200).end();
 }
