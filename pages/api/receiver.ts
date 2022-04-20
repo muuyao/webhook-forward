478fd5c0cf865d31c6ae23e253329d298427ac88
wangxiao <wanngxiao@gmail.com>
2022-04-20 11:53:51 +0800

    feat: fix gitlab hook

--- pages/api/receiver.ts
+++ pages/api/receiver.ts
@@ -3,14 +3,13 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 type Data = {
-  name: string;
+  success: boolean;
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const body = req.body;
+  const body = JSON.parse(req.body);
   const headers = req.headers;
-
-  if (headers['X-Gitlab-Event']) {
+  if (headers['x-gitlab-event']) {
     const { event_type, user, repository, object_attributes } = body;
     await axios.post('https://open.feishu.cn/open-apis/bot/v2/hook/db3c5890-fe9d-451f-818d-0d06193c5c28', {
       msg_type: 'post',
@@ -37,5 +36,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     });
   }
 
-  res.status(200).end();
+  res.status(200).json({
+    success: true,
+  });
 }
